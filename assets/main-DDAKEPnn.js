f21d6b98b4ef1fc22096e13b67e2e9574b480ab2
DOSHICK <73038931+DOSHICK@users.noreply.github.com>
2025-04-27 20:00:47 +0300

    123123

---
+++ assets/main-DDAKEPnn.js
@@ -0,0 +1,1 @@
+import{c as r,j as e,B as t,A as o}from"./App-Yst4Mkbz.js";r.createRoot(document.getElementById("root")).render(e.jsx(t,{basename:"",children:e.jsx(o,{})}));
---
+++ assets/win-JjwL8LX9.js
@@ -0,0 +1,1 @@
+import{c as r,j as e,B as t,A as s}from"./App-Yst4Mkbz.js";r.createRoot(document.getElementById("root")).render(e.jsx(t,{basename:"",children:e.jsx(s,{isAppWin:!0})}));
